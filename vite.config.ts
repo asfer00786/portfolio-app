c2e1323186ee44eee4cdbe1546f54d5d950c087e
Night Owl Coder <asferjutt78@gmail.com>
2026-04-10 22:59:59 +0300

    Update base configuration to '/portfolio-app/'

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
-})
+  base: '/portfolio-app/',
+  // other configurations
+});
